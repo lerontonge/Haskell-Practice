558d1d7fb5ff54b81a062cae5e0c303075e2a13f
Leron T <lerontonge@gmail.com>
2021-04-11 18:56:16 -0400

    chapter 6

--- SocketsAndPlugsCH6.hs
+++ SocketsAndPlugsCH6.hs
@@ -1,2 +1,30 @@
 module SocketsAndPlugsCH6 where
 
+plus5 :: Int -> Int 
+plus5 x = x + 5
+
+plus6 :: Int -> Int
+plus6 y = y + 6
+
+plus7 :: Int -> Int 
+plus7 z = z + 7
+
+plus8 :: Int -> Int -> Int -> Int 
+plus8 = \x y z -> x + y + z + 8
+
+--result :: IO()
+--result = print plus8 (plus7 (plus6 (plus5 5)))
+
+plus6'' :: Int -> Int 
+plus6'' number = (6+) number
+
+plusSeven :: Int -> Int 
+plusSeven = (+7)
+
+sumFrom1To :: Integral => a -> a
+sumFrom1To 0 = 0
+sumFrom1To n = n + sumFrom1To(n-1)
+
+isEven :: Integral a => a -> Bool
+isEven n = mod n 2 == 0 
+_
