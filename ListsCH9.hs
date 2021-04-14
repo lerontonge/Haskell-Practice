4d9c5d17b3f8b238eec064c219735ce237a3fa7e
Leron T <lerontonge@gmail.com>
2021-04-13 22:39:55 -0400

    Lists CH9

--- ListsCH9.hs
+++ ListsCH9.hs
@@ -7,15 +7,15 @@ aList :: [a]
 aList = []
 
 -- A list with the string Sauce in it 
-aList2 = "sauce" : []
+aList2 = " Sauce" : []
 
 -- A list with the : used a prefix
 aList3 :: [[Char]]
-aList3 = (:)"pie" []
+aList3 = (:)" Pie" []
 
 --The usual way to make a list
 aList4 :: [String]
-aList4 = ["Napkin"]
+aList4 = [" Napkin"]
 
 shoppingList :: [String]
 shoppingList = ["Carrots"
@@ -29,21 +29,32 @@ shoppingList = ["Carrots"
                ,"Broccoli"
                ]
 
-main :: IO()
-main = print shoppingList
+main'' :: IO()
+main'' = print shoppingList
 
 
-main'' :: IO ()
-main'' = print (show(length shoppingList))
+main :: IO ()
+main = putStrLn ("There are " ++ show(length shoppingList) ++ " items in the Shopping List. " ++ joinedWithCommas shoppingList ++ joinedWithCommas aList9 ++ joinedWithCommas aList4)
 
-aList9 :: [] String 
-aList9 = ["Cat","Food","Lasagne"]
+joinedWithCommas :: [String] -> String  
+joinedWithCommas [] = ""
+joinedWithCommas [x] = x 
+joinedWithCommas (x:xs) = x ++", " ++ joinedWithCommas xs 
+
+aList9 :: [String] 
+aList9 = [" Cat","Food","Lasagne"]
 
 main' :: IO()
 main' = print (aList9 ++ shoppingList)
 
 lists :: [String]
 lists = aList9 ++ aList4 ++ aList3
+
+
+firstOrEmpty :: [String] -> String 
+firstOrEmpty [] = "" 
+firstOrEmpty (x:_) = x
+
 
 
 
