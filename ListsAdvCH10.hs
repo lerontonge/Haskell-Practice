2555519d2f14ede2b0e47a66ccf5722561ae3ce2
Leron T <lerontonge@gmail.com>
2021-04-13 23:39:36 -0400

    Chapter 10

---
+++ ListsAdvCH10.hs
@@ -0,0 +1,18 @@
+module ListsAdvCH10 where
+
+movies :: [String]
+movies =
+  [ "Aeon Flux"
+  , "The Black Cat"
+  , "Superman"
+  , "Stick It"
+  , "The Matrix Revolutions"
+  , "The Raven"
+  , "Inception"
+  , "Looper"
+  , "Hoodwinked"
+  , "Tell-Tale"
+  ]
+
+  
+
