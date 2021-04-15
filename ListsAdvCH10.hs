f801a0e7386530af205afaf9dcd32e42fb96da5d
Leron T <lerontonge@gmail.com>
2021-04-15 06:00:39 -0400

    Chapter 10

--- ListsAdvCH10.hs
+++ ListsAdvCH10.hs
@@ -1,5 +1,7 @@
 module ListsAdvCH10 where
 
+import qualified Data.List as L
+
 movies :: [String]
 movies =
   [ "Aeon Flux"
@@ -12,7 +14,25 @@ movies =
   , "Looper"
   , "Hoodwinked"
   , "Tell-Tale"
-  ]
+    ]
 
-  
+isGood :: String -> Bool 
+isGood (x:_) = x <= 'M'
+isGood _     = False 
+
+assess :: String -> String 
+assess movie = movie ++ " - " ++ assessment 
+where assessment if isGood movie
+                       then "Good"
+                       else "Bad" 
+
+assessMovies :: [String] -> [String]
+assessMovies = map assess
+
+assessedMovies :: [String]
+assessedMovies = assessMovies movies
+
+
+
+
 
