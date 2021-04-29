4db95d1f8d2e37e33656b7ec0c9d2d56159b5c8f
Leron T <lerontonge@gmail.com>
2021-04-28 23:08:52 -0400

    main

--- CatsAndHouses.hs
+++ CatsAndHouses.hs
@@ -19,9 +19,31 @@ type HouseNumber = Int
 data House = House HouseNumber Cat
 
 humanAge :: Cat -> Age
-humanAge = Cat _ _ catAge 
-         | catAge <= 0 = 0
-         | catAge == 1 = 15
-         | catAge == 2 = 25
-         | otherwise  = 25 + (catAge - 2) * 4
+humanAge (Cat _ _ catAge) 
+             | catAge <= 0 = 0
+             | catAge == 1 = 15
+             | catAge == 2 = 25
+             | otherwise  = 25 + (catAge - 2) * 4
+
+street :: [House]
+street = 
+    [ House 1 (Cat "George" Siamese 10)
+    , House 2 (Cat "Mr Bigglesworth" Persian 5)
+    , House 3 (Cat "Mr Tinkles" Birman 1)
+    , House 4 (Cat "Puddy" Burmese 3)
+    , House 5 (Cat "Tiger" Bengal 12)
+    , House 6 (Cat "The Ninja" RussianBlue 12)
+    , House 7 (Cat "Mr Tinklestein" NorwegianForest 8)
+    , House 8 (Cat "Plain Cat" MainCoon 9 )
+    , House 9 (Cat "Shabooby" Sphynx 7)
+    , House 10 (Cat " Crazy Ears Sam" CornishRex 3) ]
+    
+getCatFromHouse :: House -> Cat 
+getCatFromHouse = (House _ Cat)
+
+getHumanAgeFromCat :: House -> Cat
+getHumanAgeFromCat = humanAge . getCatFromHouse
+
+                                                                                                                                                                                                                                                                                                                                         `
+
 
