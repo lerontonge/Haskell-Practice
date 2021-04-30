39cbea72f13f2d43cb1fdb1ee4e1ad02b5e8621b
Leron T <lerontonge@gmail.com>
2021-04-29 23:56:30 -0400

    main

--- TomatoFarm.hs
+++ TomatoFarm.hs
@@ -7,6 +7,9 @@ data StrainType = Indica
                 | Hybrid 
                 | Ruderalis
 
+data FloweringType = Photoperiod
+                   | AutoFlower                
+
                 
 
 
