eae9d43829054c467d8870f96ca304b1d0c98580
Leron T <lerontonge@gmail.com>
2021-05-14 00:43:16 -0400

    practice

--- ThePeopleBookCH17.hs
+++ ThePeopleBookCH17.hs
@@ -5,8 +5,11 @@ type Name = String
 type Year = Int
 type Weight = Float
 type EyeColor = String
+
+-- Person type written in record syntax
 data Person = Person
     { personFirstName :: Name,
+      personMiddleName :: Name,
       personLastName :: Name,
       personWeight :: Weight,
       personEyeColor :: EyeColor,
@@ -21,13 +24,13 @@ blaise = Person { personFirstName = "Blaise",
                   yearOfBirth = 1623} -}
 
 people :: [Person]
-people = [ Person "Issac" "Newton" 180 "Green" 1643,
-           Person "Leonard" "Euler" 192 "Brown" 1707,
-           Person "Ada" "Lovelace" 123 "Black" 1815,
-           Person "Alan" "Turing" 198 "Blue" 1912,
-           Person "Haskell" "Curry" 200 "Black" 1900,
-           Person "John" "von Neumann" 180 "Green" 1903,
-           Person "Lipot" "Fejer" 175 "Black" 1880]
+people = [ Person "Issac" "Jimbo" "Newton" 180 "Green" 1643,
+           Person "Leonard" "James" "Euler" 192 "Brown" 1707,
+           Person "Ada" "Lou" "Lovelace" 123 "Black" 1815,
+           Person "Alan" "Alex" "Turing" 198 "Blue" 1912,
+           Person "Haskell" "Jermaine" "Curry" 200 "Black" 1900,
+           Person "John" "Delroy" "von Neumann" 180 "Green" 1903,
+           Person "Lipot" "Kevin" "Fejer" 175 "Black" 1880]
 
 -- Chapter 17.6
 {- firstAfter1900 :: Maybe Person
@@ -72,7 +75,43 @@ peopleToFirstName :: [Person] -> [String]
 peopleToFirstName [] = []
 peopleToFirstname (x:xs) = personFirstName x : peopleToFirstName xs
 
+-- getting a list of first names
+
+lastNames :: [String]
+lastNames = map personLastName people
+
+
+--getting a list of last lastNames
+
 firstNames :: [String]
 firstNames = map personFirstName people 
 
+--sorting a list in alphabetical order
+
+sortedLastNames :: [String]
+sortedLastNames = L.sort lastNames
+
+--Sorting a list in reverse alphbetical order
+
+reverseSortedLastNames :: [String]
+reverseSortedLastNames = L.sortBy reverseCompare lastNames
+  where reverseCompare = flip compare
+
+-- Sorting the list by a field in the list using ( SortOn )
+
+sortedPeopleByFirstNames :: [Person]
+sortedPeopleByFirstNames = L.sortOn personFirstName people 
+
+-- Function that takes a year and a person and works out how many years ago from that year that person was born
+
+yearsSinceBirthYear :: Year -> Person -> Int 
+yearsSinceBirthYear currYear person = currYear - yearOfBirth person 
+
+-- Mapping the list 'people' to prior function
+wasBornYearsAgo :: [Int]
+wasBornYearsAgo = map (yearsSinceBirthYear 2021) people 
+
+
+earliestYearOfBirth :: [Person] -> Int
+earliestYearOfBirth people = minimum $ map yearOfBirth people 
 
