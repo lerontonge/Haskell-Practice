694ee3114209266fc9e67bdb281f40ee8229e9cf
Leron T <lerontonge@gmail.com>
2021-06-09 11:18:22 -0400

    practice

--- SkwakTheSquirrelCH19.hs
+++ SkwakTheSquirrelCH19.hs
@@ -1,5 +1,9 @@
 module SkwakTheSquirrelCH19 where
 
+import qualified Data.List as L 
+
+
+-- Custom Type definitions
 data GameObject = Player
                 | Acorn
     deriving (Eq, Show)
@@ -7,7 +11,7 @@ data GameObject = Player
 data Room = 
     Room Description [GameObject]
         deriving (Show)
-        
+--Type Aliases        
 type Description = String 
 type Inventory = [GameObject]
 type GameMap = [Room]
@@ -28,15 +32,144 @@ main = do
 gameLoop :: GameState ->IO()
 gameLoop (rooms, currentInv) = do
     let currentRoom = 
-        case findRoomWithPlayer rooms of 
-        Just r -> r
-        Nothing -> error $ "Somehow the player " 
-                         ++ "ended up outsode the map"
+            case findRoomWithPlayer rooms of 
+            Just r -> r
+            Nothing -> error $ "Somehow the player " 
+                         ++ "ended up outside the map"
         possibleCmds =
             validCommands currentRoom currentInv
     if playerWon (rooms, currentInv)
-        then gameOverReseart
-        else do
+    then gameOverRestart
+    else do
             describeWorld currentRoom currentInv possibleCmds takeActionThenLoop
                 currentRoom currentInv possibleCmds rooms
                         
+findRoomWithPlayer :: [Room] -> Maybe Room 
+findRoomWithPlayer rooms = L.find (\(Room _ obs) -> any (==Player)obs)rooms 
+
+validCommands :: Room -> Inventory -> [String]
+validCommands (Room _ gameObjs) invItems = ["go"] ++ takeCommandList ++ ["quit"]
+    where takeCommandList = if somethingToTake gameObjs
+                            then ["take"]
+                            else[]
+          dropCommandlist = if length invItems > 0
+                            then ["put"]
+                            else[]  
+
+somethingToTake :: [GameObject] -> Bool 
+somethingToTake objs = any (/= Player) objs
+
+
+playerWon :: GameState -> Bool 
+playerWon (rooms, currentInv) = any hasAcornAndInside rooms
+    where hasAcornAndInside (Room desc objs) = 
+            desc == "You are inside a tree." 
+            && any (==Acorn) objs
+
+gameOverRestart :: IO()
+gameOverRestart = do
+    putStrLn $ "You Won!"
+        ++ "You have successfully stored the acorn"
+        ++ " for winter. Well done!"
+    putStrLn "Do you want to play again? y = yes"
+    playAgain <- getLine
+    if playAgain == "y"
+    then gameLoop initialState
+    else putStrLn "Thanks for playing"
+
+getCommand :: IO String
+getCommand = do
+    putStrLn "What do you want to do?"
+    getLine
+
+takeActionThenLoop :: Room ->
+                  Inventory ->
+                  [String] ->
+                  [Room] ->
+                  IO ()
+takeActionThenLoop currentRoom
+               currentInv
+               possibleCmds
+               rooms =
+                        do
+                            command <- getCommand 
+                            if any (==command) possibleCmds
+                            then case command of
+                                "go" -> 
+                                    do
+                                        putStrLn "You go..."
+                                        gameLoop $ movePlayer (rooms, currentInv)
+                                "take" ->
+                                    do 
+                                        putStrLn "You take the acorn..."
+                                        gameLoop $ moveAcornToInventory (rooms, currentInv)
+                                "put" ->
+                                    do
+                                        putStrLn "You put the acorn down..."
+                                        gameLoop $ moveAcornFromInventory (rooms, currentInv)
+                                "quit" -> 
+                                    do 
+                                        putStrLn $ "You decide to give up." ++ "Thanks for playing"
+
+                                _ -> 
+                                    do
+                                        putStrLn "That is not a command."
+                                        gameLoop(rooms, currentInv)
+                            else do
+                                putStrLn $ "Command not possible here," ++ " or that is not a command"
+                                gameLoop (rooms, currentInv)
+movePlayer :: GameState -> GameState
+movePlayer (rooms, inv) = (newRooms, inv)
+    where newRooms = map adjustRooms rooms
+          adjustRooms (Room d objs) = 
+              if any (==Player) objs
+              then (Room d (filter (/=Player) objs))
+              else (Room d (Player : objs))
+
+moveAcornToInventory :: GameState -> GameState
+moveAcornToInventory (rooms, inv) = (newRooms, newInv)
+    where 
+        newInv = Acorn : inv
+        newRooms = map adjustRooms rooms
+        adjustRooms (Room d objs) = Room d $ filter(/=Acorn)objs
+
+moveAcornFromInventory :: GameState -> GameState
+moveAcornFromInventory (rooms, inv) = (newRooms, newInv)
+    where
+        newInv = filter (/=Acorn) inv 
+        newRooms = map adjustRooms rooms
+        adjustRooms (Room d objs) = 
+            if any (==Player) objs
+            then Room d (Acorn:objs)
+            else Room d objs
+
+describeWorld :: Room ->
+                 Inventory ->
+                 [String] ->
+                 IO ()
+describeWorld currentRoom
+              currentInv
+              possibleCmds =
+    do
+        putStrLn $ describeRoom currentRoom
+        putStrLn $ describeInventory currentInv
+        putStrLn $ describeCommands possibleCmds 
+
+describeRoom :: Room -> String 
+describeRoom (Room desc objs) = 
+    desc ++ if any(==Acorn) objs
+            then " There is an acorn here"
+            else ""
+
+describeInventory :: Inventory -> String
+describeInventory [] =  "You are holding nothing"
+describeInventory inv = "You are holding: " ++  (concat $ map show inv) 
+
+describeCommands :: [String] -> String
+describeCommands commands = "Commands: " ++ (L.intercalate ", " commands)
+
+
+
+
+
+
