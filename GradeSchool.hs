5ca25f001929391a1f74b53ff01d97a4f97f1079
Leron T <lerontonge@gmail.com>
2021-06-09 03:36:14 -0400

    practice

---
+++ GradeSchool.hs
@@ -0,0 +1,23 @@
+module School (School, add, empty, grade, sorted) where
+
+
+
+
+data School = Dummy
+
+
+
+add :: Int -> String -> School -> School
+add gradeNum student school = 
+    
+
+empty :: School
+empty = 
+
+
+grade :: Int -> School -> [String]
+grade gradeNum school = 
+
+
+sorted :: School -> [(Int, [String])]
+sorted school = 
