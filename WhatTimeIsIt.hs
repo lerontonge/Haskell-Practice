b92a456884b2725e7d7dc2b690f33860d7d8db53
Leron T <lerontonge@gmail.com>
2021-06-10 19:13:41 -0400

    practice

--- WhatTimeIsIt.hs
+++ WhatTimeIsIt.hs
@@ -1,1 +1,16 @@
 module WhatTimeIsIt where
+
+whatTimeIsIt :: IO String 
+whatTimeIsIt = do
+    putStrLn "What time is it now?"
+    getLine
+
+main :: IO()
+main = do
+    timeString <- whatTimeIsIt
+    putStrLn "Again!"
+    timeString2 <- whatTimeIsIt
+    putStrLn ("Ok, you said it was "
+              ++ timeString
+              ++ " and then you said it was"
+              ++ timeString2)
