63e65714838920d3208fa0736af666f1c7b83d30
Leron T <lerontonge@gmail.com>
2021-06-10 02:16:13 -0400

    practice

---
+++ BasicInput2.hs
@@ -0,0 +1,10 @@
+main ::  IO()
+main = do
+    putStrLn "What is your name?"
+    name <- getLine
+    putStrLn "What is your pet's name?"
+    petName <- getLine 
+    putStrLn ("Hello " ++ name ++ " Your pet's name is " ++ petName)
+
+
+
