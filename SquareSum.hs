180d64436e48af5e87ba47d883a58a86e98cf3c2
Leron T <lerontonge@gmail.com>
2021-06-13 09:37:13 -0400

    practice

---
+++ SquareSum.hs
@@ -0,0 +1,9 @@
+module SquareSum where
+
+squareSum :: [Integer] -> Integer
+squareSum [] = 0
+squareSum (x:xs) = square x + squareSum xs 
+                      where square x = x*x
+
+-- OR
+-- squareSum (x:xs) = x ^ 2 + squareSum xs 
