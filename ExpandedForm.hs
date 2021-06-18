59394982200d77d3a764e37b8b25b9aa77137de9
Leron T <lerontonge@gmail.com>
2021-06-18 12:56:08 -0400

    codewars

--- ExpandedForm.hs
+++ ExpandedForm.hs
@@ -1,16 +1,10 @@
-import Numeric
-import Data.String
-import Numeric.Natural
+import Numeric ()
+import Data.String ()
+import Numeric.Natural ()
 import Data.Char ( digitToInt )
-import Data.List
-import Data.Maybe
+import Data.List ()
+import Data.Maybe ()
 
-
-testNum :: Int
-testNum = 123456
-
-testList :: [Int]
-testList = [1,2,3,4,5,6]
 
 digitsToList :: Int -> [Int]
 digitsToList 0 = []
@@ -19,11 +13,8 @@ digitsToList x = digitsToList (x `div`10) ++ [ x `mod` 10]
 expandDigits :: Int -> Int
 expandDigits x = 10^x 
 
-numList :: [Int]
-numList = digitsToList testNum
 expLs :: [Int] -> [Int]
 expLs xs = reverse (map expandDigits [0..length xs-1])
-
 
 multiplyLists :: [Int] -> [Int] -> [Int]
 multiplyLists x y = zipWith (*) y x 
