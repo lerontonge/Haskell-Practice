69e50144792ab1f8ac5a7a2ab16323d9a90da66f
Leron T <lerontonge@gmail.com>
2021-06-18 23:07:47 -0400

    codewars

--- ExpandedForm.hs
+++ ExpandedForm.hs
@@ -2,7 +2,7 @@ import Numeric ()
 import Data.String ()
 import Numeric.Natural ()
 import Data.Char ( digitToInt )
-import Data.List ()
+import Data.List ( intercalate )
 import Data.Maybe ()
 
 
@@ -11,20 +11,27 @@ digitsToList 0 = []
 digitsToList x = digitsToList (x `div`10) ++ [ x `mod` 10]
 
 expandDigits :: Int -> Int
-expandDigits x = 10^x 
+expandDigits x = 10^x
 
 expLs :: [Int] -> [Int]
 expLs xs = reverse (map expandDigits [0..length xs-1])
 
 multiplyLists :: [Int] -> [Int] -> [Int]
-multiplyLists x y = zipWith (*) y x 
+multiplyLists x y = zipWith (*) y x
 
-expandedForm' :: [Int] -> String 
+expandedForm' :: [Int] -> String
 expandedForm' [] = []
+expandedForm' [0] = []
 expandedForm' [x] = show x
-expandedForm' (x:xs) =  show x ++ " + " ++ expandedForm' xs
+expandedForm' (x:xs) | x > 0 = show x ++ " + " ++ expandedForm' xs 
+                     | otherwise = expandedForm' xs
 
 
 expandedForm :: Int -> String
 expandedForm n = expandedForm' (multiplyLists expandedList (digitsToList n))
-            where expandedList =expLs (digitsToList n)
+            where expandedList = expLs (digitsToList n)
+
+
+test :: Int -> String
+test n = intercalate " + " multiplyLists expandedList (digitsToList n)
+        where expandedList = expLs (digitsToList n) 
