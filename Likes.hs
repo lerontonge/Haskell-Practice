1431d800beccced2d58d53981a698a869ed334cf
Leron T <lerontonge@gmail.com>
2021-06-21 06:29:15 -0400

    codewars

--- Likes.hs
+++ Likes.hs
@@ -3,4 +3,6 @@
 likes :: [String] -> String
 likes  [] = "no one likes this"
 likes  [x] = x ++ " likes this"
-likes  (x:y:_) = x ++ " and " ++ y ++ " like this"
+likes  (x:y:xs) 
+                | (length xs) >= 2 = x ++ ", "++ y ++ " and " ++ show (length xs) ++" others like this"
+                | otherwise = x ++ ", "++ y ++ " and " ++ likes xs ++ "like this"
