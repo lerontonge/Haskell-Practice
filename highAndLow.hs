7d420e51e53aa49e2a8fe61f110926bed51062b3
Leron T <lerontonge@gmail.com>
2021-06-24 19:50:24 -0400

    codewars

--- highAndLow.hs
+++ highAndLow.hs
@@ -5,7 +5,7 @@ stringToInt :: String -> [Int]
 stringToInt strlst = map read (words strlst) :: [Int]
 
 minAndMax :: (Foldable t, Ord a) => t a -> [a]
-minAndMax xs =  [(minimum xs) , (maximum xs)]
+minAndMax xs =  [minimum xs , maximum xs]
 
 highAndLow :: String -> String
 highAndLow input =  unwords (map show (minAndMax(stringToInt input)))
