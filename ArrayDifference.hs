636034067f6715dbcdbb83ff2956fb05fe5ba626
Leron T <lerontonge@gmail.com>
2021-06-25 22:24:30 -0400

    codewars

--- ArrayDifference.hs
+++ ArrayDifference.hs
@@ -1,5 +1,5 @@
 
--- takes 2 lists and returns the differens between them
+-- takes 2 lists and returns the difference between them
 
 difference :: (Foldable t, Eq a) => [a] -> t a -> [a]
 difference  xs ys = filter (`notElem` ys) xs 
