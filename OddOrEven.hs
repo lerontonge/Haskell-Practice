5e9b09253a623e89c5fb7c423b536e1c9c4cf0c2
Leron T <lerontonge@gmail.com>
2021-06-28 18:51:29 -0400

    codewars

--- OddOrEven.hs
+++ OddOrEven.hs
@@ -1,11 +1,11 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 
 
 
 oddOrEven :: Integral a => [a] -> [Char]
-oddOrEven xs    
-                | xs == []  = "even"
-                | xs == [0] = "even"
-                | abs (sum xs  `mod` 2) == 1 = "odd"
-                | abs (sum xs  `mod` 2) == 0 = "even"
-                
+oddOrEven lst   | null lst  = "even"
+                | lst == [0] = "even"
+                | abs (sum lst  `mod` 2) == 1 = "odd"
+                | abs (sum lst  `mod` 2) == 0 = "even"
+
