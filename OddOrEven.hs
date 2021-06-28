4b50055e3212b5e4422e7264ceaf09674cb8f6ec
Leron T <lerontonge@gmail.com>
2021-06-28 00:02:08 -0400

    codewars

--- OddOrEven.hs
+++ OddOrEven.hs
@@ -1,6 +1,11 @@
 
 
 
-oddOrEven xs 
+
+oddOrEven :: Integral a => [a] -> [Char]
+oddOrEven xs    
+                | xs == []  = "even"
+                | xs == [0] = "even"
                 | abs (sum xs  `mod` 2) == 1 = "odd"
                 | abs (sum xs  `mod` 2) == 0 = "even"
+                
