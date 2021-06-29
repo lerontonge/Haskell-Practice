1edef3b9de032b5acf6df00fb2070f1445130ff6
Leron T <lerontonge@gmail.com>
2021-06-28 22:07:51 -0400

    codewars

---
+++ AllStar.hs
@@ -0,0 +1,12 @@
+data Player = Player { team :: String,
+                       ppg :: Double }
+                       deriving (Show)
+
+sumPpg :: Player -> Player -> Double
+sumPpg Player{ ppg = x } Player{ppg = y} =  x + y
+
+
+iverson :: Player
+iverson = Player { team = "76ers", ppg = 11.2 }
+jordan :: Player
+jordan  = Player { team = "bulls", ppg = 20.2 }
