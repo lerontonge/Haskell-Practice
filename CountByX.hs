06bf942ddb7e4357a72fa7b841c4b0e6a406e0c6
Leron T <lerontonge@gmail.com>
2021-06-30 19:29:47 -0400

    codewars

--- CountByX.hs
+++ CountByX.hs
@@ -1,2 +1,18 @@
-countBy :: Integer -> Int -> [Integer]
-countBy x n = 
+{-Create a function with two arguments that will return an array of the first (n) multiples of (x).
+
+Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+Return the results as an array (or list in Python, Haskell or Elixir).
+
+Examples:
+
+countBy 1 10 `shouldBe` [1,2,3,4,5,6,7,8,9,10]
+countBy 2  5 `shouldBe` [2,4,6,8,10] -}
+
+
+
+
+
+countBy :: (Num b, Enum b) => b -> b -> [b]
+countBy x n = map (x *) [1..n]
+
