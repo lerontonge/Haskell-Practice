32f8dbf259e705fc8340d774f8c16644e8667f47
Leron T <lerontonge@gmail.com>
2021-06-30 20:34:13 -0400

    codewars

--- SumTheStrings.hs
+++ SumTheStrings.hs
@@ -11,9 +11,7 @@ If either input is an empty string, consider it as zero.
 Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)-}
 
 sumStr :: String -> String -> String
-sumStr str1 str2 = show (x+y)
-    where 
-        x :: Int 
-        x = read str1 
-        y :: Int
-        y = read str2
+sumStr "" "" =  show ((read "0" :: Int) + (read "0" :: Int))
+sumStr str1 "" = show ((read "0" :: Int) + (read str1 :: Int))
+sumStr "" str2 = show ((read "0" :: Int) + (read str2 :: Int))
+sumStr str1 str2 = show ((read str1 :: Int) + (read str2 :: Int))
