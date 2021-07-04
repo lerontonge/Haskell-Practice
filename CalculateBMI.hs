15fe5fd07ec3b3ed61508f4d02a499277e38b5b9
Leron T <lerontonge@gmail.com>
2021-07-03 22:33:59 -0400

    codewars

---
+++ CalculateBMI.hs
@@ -0,0 +1,7 @@
+
+bmi :: Float -> Float -> String  
+bmi weight height | bmi' <= 18.5 = "Underweight"
+                  | bmi' <= 25.0 && bmi' > 18.5 = "Normal"
+                  | bmi' <= 30.0 && bmi' > 25.0 = "Overweight"
+                  | otherwise = "Obese"
+                   where bmi' = weight / (height * height)
