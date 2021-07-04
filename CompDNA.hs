4328e167d0f4bcd771234cc8ef1a8940bddda3a7
Leron T <lerontonge@gmail.com>
2021-07-03 22:01:26 -0400

    codewars

---
+++ CompDNA.hs
@@ -0,0 +1,14 @@
+
+
+
+complimentary :: Char -> Char
+complimentary c = case c of 
+    'A' -> 'T'
+    'T' -> 'A'
+    'G' -> 'C'
+    'C' -> 'G' 
+
+
+dnaStrand :: [Char] -> [Char]
+dnaStrand = map complimentary
+
