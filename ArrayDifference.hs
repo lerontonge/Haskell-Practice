237fe08d1bbe6a10cc52f477d0ab2e4cacca1b10
Leron T <73184944+lerontonge@users.noreply.github.com>
2021-07-06 13:29:21 -0400

    testcommit
    Testing Windows github setup

--- ArrayDifference.hs
+++ ArrayDifference.hs
@@ -1,5 +1,7 @@
 
 -- takes 2 lists and returns the difference between them
 
+--Windows VS Code commit Test
+
 difference :: (Foldable t, Eq a) => [a] -> t a -> [a]
 difference  xs ys = filter (`notElem` ys) xs 
--- BasicInput2.hs
+++ BasicInput2.hs
@@ -8,3 +8,4 @@ main = do
 
 
 
+--Test Commit
