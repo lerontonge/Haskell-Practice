a507b301fa8af428a58fdc0261152474c570215e
Leron T <lerontonge@gmail.com>
2021-09-18 21:49:22 -0400

    HaskellPrinciplesCh3

--- HPFPCh3.hs
+++ HPFPCh3.hs
@@ -1,6 +1,7 @@
 module HPFPCh3 where
 
 import Text.Parsec.Char (letter)
+
 curry' :: String
 curry' = "Curry is awesome"
 
@@ -8,8 +9,15 @@ curry' = "Curry is awesome"
 thirdLetter :: String -> Char 
 thirdLetter x = x !! 2
 
-
-
+letterIndex :: Int -> Char 
+letterIndex x = curry' !! (x - 1)
+ 
+rvrs :: String 
+rvrs = wrd3 ++ wrd2 ++ wrd1
+        where wrd1 = take 5 curry'
+              wrd3 = drop 9 curry'
+              wrd2 = drop 5 wrd4
+                    where wrd4 = take 9 curry' 
 
 main :: IO()
 main = do
