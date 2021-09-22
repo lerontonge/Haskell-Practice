fc87a0573d4f773075d936375e3afdc7c54d90b9
Leron T <lerontonge@gmail.com>
2021-09-22 10:36:05 -0400

    HPFPCh4

---
+++ MoodSwing.hs
@@ -0,0 +1,11 @@
+module MoodSwing where
+import Prelude (Show)
+--import MoodSwing (Mood(Woot))
+
+data Mood = Blah | Woot deriving Show
+
+changeMood :: Mood -> Mood 
+changeMood Blah = Woot 
+changeMood Woot = Blah  
+
+
