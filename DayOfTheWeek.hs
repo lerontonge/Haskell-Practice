9de24fae17692f656255e9f0148e31cc27bd184b
Leron T <lerontonge@gmail.com>
2021-10-26 01:44:16 -0400

    main

--- DayOfTheWeek.hs
+++ DayOfTheWeek.hs
@@ -26,3 +26,35 @@ data Identity a = Identity a
 
 instance Eq a => Eq (Identity a) where 
     (==)(Identity v) (Identity v') = v == v'
+
+
+data TisAnInteger = TisAn Integer 
+
+instance Eq TisAnInteger where
+    TisAn x == TisAn y  = True 
+
+
+
+data TwoIntegers = Two Integer Integer 
+
+instance Eq TwoIntegers where
+    Two x y == Two w z = True 
+
+
+data StringOrInt = TisAnInt Int | TisAString String 
+instance Eq StringOrInt where
+    (==) (TisAnInt x) (TisAnInt y) = x == y 
+    (==) (TisAString x) (TisAString y) = True
+    (==) _ _ = False 
+
+data Pair a = Pair a a 
+instance Eq a => Eq (Pair a) where
+    (==) (Pair a b)  (Pair a' b') = True  
+
+
+data Tuple a b = Tuple a b 
+instance (Eq a, Eq b) => Eq (Tuple a b) where
+    (==) (Tuple c d) (Tuple a b) = c == a && d == b 
+
+
+
