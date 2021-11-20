8370a149f0ec215b057be9d9c8968ab6fd314030
Leron T <lerontonge@gmail.com>
2021-11-20 07:32:07 -0400

    main

---
+++ RegisteredUser2.hs
@@ -0,0 +1,11 @@
+module RegisteredUser2 where
+
+newtype Username = Username String 
+
+newtype AccountNumber = AccountNumber Integer 
+
+data User = UnregisteredUser | RegisteredUser Username AccountNumber 
+
+printUser :: User -> IO ()
+printUser UnregisteredUser = putStrLn "Unregistered User"
+printUser (RegisteredUser (Username name)(AccountNumber acctNum)) = putStrLn $ name ++ " " ++ show acctNum 
