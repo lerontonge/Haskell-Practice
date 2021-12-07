aed47bcf17db147e5f3d9a8611618d0c5b3e2f50
lerontonge <lerontonge@gmail.com>
2021-12-06 20:36:22 -0400

    haskellbook

--- HowFunctionsWork.hs
+++ HowFunctionsWork.hs
@@ -27,9 +27,9 @@ a) Rewrite the f function in the where clause.-}
 addOneIfOdd n = case odd n of 
     True  -> f n
     False -> n
-    where f n = n +1
+    where f n = n + 1
 
-addOneIfOdd' n = case odd of
+addOneIfOdd' n = case odd n of
     True -> f n
     False -> n
     where f = \n -> n + 1
