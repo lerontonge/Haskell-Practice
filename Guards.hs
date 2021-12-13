270447f2f0d42bab80e8dac6608ff6ea2b2e5806
lerontonge <lerontonge@gmail.com>
2021-12-12 21:51:06 -0400

    haskellbook

--- Guards.hs
+++ Guards.hs
@@ -1,3 +1,4 @@
+import Distribution.Simple.Utils (xargs)
 
 
 
@@ -11,4 +12,18 @@ bloodNa x
     | x < 135 = "too low"
     | x > 145 = "too high"
     | otherwise = "normal"
+
+isRight :: (Num a, Eq a) => a -> a -> a -> [Char]
+isRight a b c 
+    | a^2 + b^2 == c^2 = "Right on"
+    | otherwise = "Wrong"
+
+
+dogYrs :: Integer -> Integer 
+dogYrs x
+    | x <= 0 = 0
+    | x <= 1 = x * 15
+    | x <= 2 = x * 12
+    | x <= 4 = x * 8 
+    | otherwise = x * 6
 
