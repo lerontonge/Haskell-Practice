23b9cfbb25e7a1a7fd76fa2bae4180cc5cb5ec2e
lerontonge <lerontonge@gmail.com>
2021-12-13 09:53:47 -0400

    haskellbook

--- Guards.hs
+++ Guards.hs
@@ -27,3 +27,12 @@ dogYrs x
     | x <= 4 = x * 8 
     | otherwise = x * 6
 
+avgGrade :: (Ord a, Fractional a) => a -> Char
+avgGrade x
+    | y >= 0.9 = 'A'
+    | y >= 0.8 = 'B'
+    | y >= 0.7 = 'C'
+    | y >= 0.59 = 'D'
+    | y < 0.59 = 'F'
+    where y = x / 100
+
