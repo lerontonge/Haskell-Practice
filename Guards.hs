ec904a45602d43e6bcc2589f674d0007e9077f51
lerontonge <lerontonge@gmail.com>
2021-12-16 04:21:22 -0400

    haskellbook

--- Guards.hs
+++ Guards.hs
@@ -29,18 +29,24 @@ dogYrs x
 
 avgGrade :: (Ord a, Fractional a) => a -> Char
 avgGrade x
-    | y >= 0.9 = 'A'
-    | y >= 0.8 = 'B'
-    | y >= 0.7 = 'C'
+    | y >= 0.9  = 'A'
+    | y >= 0.8  = 'B'
+    | y >= 0.7  = 'C'
     | y >= 0.59 = 'D'
-    | y < 0.59 = 'F'
-    |otherwise = 'F'
+    | y < 0.59  = 'F'
+    |otherwise  = 'F'
     where y = x / 100
 
+pal :: Eq a => [a] -> Bool
 pal xs
     | xs == reverse xs = True 
     | otherwise        = False 
 
 
+numbers :: (Ord a, Num a, Num p) => a -> p
+numbers x 
+    | x < 0 = -1
+    | x == 0 = 0
+    | x > 0 = 1
 
 
